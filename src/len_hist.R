93b5bcf4836401fe933b505b8caf711a114ca370
LOBER2814 <76394058+LOBER2814@users.noreply.github.com>
2021-06-23 00:17:52 +0300

    Histogram of lengths

---
+++ src/len_hist.R
@@ -0,0 +1,35 @@
+
+source('lib.R')
+
+###
+
+# NAME <- 'H3K36me3_K562.ENCFF903ZMQ.hg19'
+NAME <- 'H3K36me3_K562.ENCFF901ACN.hg19'
+
+###
+
+bed_df <- read.delim(paste0(DATA_DIR, NAME, '.bed'), as.is = TRUE, header = FALSE)
+colnames(bed_df) <- c('chrom', 'start', 'end', 'name', 'score')
+bed_df$len <- bed_df$end - bed_df$start
+head(bed_df)
+
+
+bed_df <- bed_df %>%
+  arrange(-len) %>%
+  filter(len < 2000)
+
+#bed_df %>%
+#  select(-len) %>%
+#  write.table(file='data/H3K36me3_K562.ENCFF901ACN.hg19.filtered.bed',
+#              col.names = FALSE, row.names = FALSE, sep = '\t', quote = FALSE)
+
+
+# hist(bed_df$len)
+
+ggplot(bed_df) +
+  aes(x = len) +
+  geom_histogram() +
+  ggtitle(NAME, subtitle = sprintf('Number of peaks = %s', nrow(bed_df))) +
+  theme_bw()
+ggsave(paste0('len_hist.', NAME, '.filtered.pdf'), path = OUT_DIR)
+
