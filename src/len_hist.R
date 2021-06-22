3ad33daf6602d5dcd1063ff7325e5ddf21a7de77
LOBER2814 <76394058+LOBER2814@users.noreply.github.com>
2021-06-23 00:27:08 +0300

    Histogram of lengths

--- src/len_hist.R
+++ src/len_hist.R
@@ -3,8 +3,8 @@ source('lib.R')
 
 ###
 
-# NAME <- 'H3K36me3_K562.ENCFF903ZMQ.hg19'
-NAME <- 'H3K36me3_K562.ENCFF901ACN.hg19'
+NAME <- 'H3K36me3_K562.ENCFF903ZMQ.hg19'
+#NAME <- 'H3K36me3_K562.ENCFF901ACN.hg19'
 
 ###
 
@@ -14,16 +14,6 @@ bed_df$len <- bed_df$end - bed_df$start
 head(bed_df)
 
 
-bed_df <- bed_df %>%
-  arrange(-len) %>%
-  filter(len < 2000)
-
-#bed_df %>%
-#  select(-len) %>%
-#  write.table(file='data/H3K36me3_K562.ENCFF901ACN.hg19.filtered.bed',
-#              col.names = FALSE, row.names = FALSE, sep = '\t', quote = FALSE)
-
-
 # hist(bed_df$len)
 
 ggplot(bed_df) +
@@ -31,5 +21,5 @@ ggplot(bed_df) +
   geom_histogram() +
   ggtitle(NAME, subtitle = sprintf('Number of peaks = %s', nrow(bed_df))) +
   theme_bw()
-ggsave(paste0('len_hist.', NAME, '.filtered.pdf'), path = OUT_DIR)
+ggsave(paste0('len_hist.', NAME, '.pdf'), path = OUT_DIR)
 
