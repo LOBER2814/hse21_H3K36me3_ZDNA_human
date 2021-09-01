150762f8e12ad52ecb730d00aa79a10a041aeba6
LOBER2814 <76394058+LOBER2814@users.noreply.github.com>
2021-09-01 22:20:30 +0300

    zhunt ghistogram

--- src/len_hist.R
+++ src/len_hist.R
@@ -6,15 +6,16 @@ source('lib.R')
 #NAME <- 'H3K36me3_K562.ENCFF903ZMQ.hg19'
 #NAME <- 'H3K36me3_K562.ENCFF903ZMQ.hg38'
 #NAME <- 'H3K36me3_K562.ENCFF901ACN.hg19'
-NAME <- 'H3K36me3_K562.ENCFF901ACN.hg38'
-
+#NAME <- 'H3K36me3_K562.ENCFF901ACN.hg38'
+NAME <- 'zhunt'
 
 ###
 
 bed_df <- read.delim(paste0(DATA_DIR, NAME, '.bed'), as.is = TRUE, header = FALSE)
-colnames(bed_df) <- c('chrom', 'start', 'end', 'name', 'score')
+#colnames(bed_df) <- c('chrom', 'start', 'end', 'name', 'score')
+colnames(bed_df) <- c('chrom', 'start', 'end')
 bed_df$len <- bed_df$end - bed_df$start
-head(bed_df)
+#head(bed_df)
 
 
 ggplot(bed_df) +
